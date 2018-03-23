2e0ec81f10426e1f14059ee122dad5f212367c30
wanghaiqiang <wanghaiqiang@huimin100.cn>
2018-03-23 16:27:54 +0800

    init project

---
+++ src/main/java/com/example/springjava8/Springjava8Application.java
@@ -0,0 +1,12 @@
+package com.example.springjava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springjava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springjava8Application.class, args);
+	}
+}
